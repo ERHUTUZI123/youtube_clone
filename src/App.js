8e82d495d885febb781b855da1a25d03f1026fb1
ERHUTUZI123 <cedricliu111@gmail.com>
2025-11-24 13:43:19 +0800

    add

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ const App = () => (
         <Routes>
             <Route path="/" exact element={<Feed />} />
             <Route path="/video/:id" element={<VideoDetail />} />
+            <Route path="/search/:searchTerm/video/:id" element={<VideoDetail />} />
             <Route path="/channel/:id" element={<ChannelDetail />} />
+            <Route path="/search/:searchTerm/channel/:id" element={<ChannelDetail />} />
             <Route path="/search/:searchTerm" element={<SearchFeed />} />
         </Routes>
         </Box>
--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,9 +1,23 @@
 import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import ReactPlayer from 'react-player';
+import { Typography, Box, Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+import { Video } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
+useEffect(() => {})
+
 const VideoDetail = () => {
   return (
-    <div>VideoDetail</div>
+    <Box minHeight='95vh'>
+      <Stack direction={{ xs: 'column', md: 'row'}}>
+        <Box flex= {1}>
+          <ReactPlayer />
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
